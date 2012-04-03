a686d477f4d194c2720b66c4331ede8305a3c84f
Phelipe Buratto <buratto@atmaconsulting.com.br>
2012-04-02 22:33:22 -0300

    Commit da classe pessoa

---
+++ src/Pessoa.java
@@ -0,0 +1,28 @@
+import java.util.Calendar;
+
+public class Pessoa {
+	private String nome;
+	private String sobreNome;
+	private Calendar data;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getSobreNome() {
+		return sobreNome;
+	}
+	public void setSobreNome(String sobreNome) {
+		this.sobreNome = sobreNome;
+	}
+	
+	public Calendar getData() {
+		return data;
+	}
+	public void setData(Calendar data) {
+		this.data = data;
+	}
+}
--- src/Programa.java
+++ src/Programa.java
@@ -2,5 +2,7 @@
 public class Programa {
 	public static void main(String[] args) {
 		System.out.println("Olá mundo!");
+		
+		Pessoa pessoa = new Pessoa();
 	}
 }
